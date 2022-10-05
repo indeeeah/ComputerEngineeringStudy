be7b350b2cbfddaed909290d6e6384f4565f12e9
KIM-SOOMINI <soomin@e-flying.co>
2022-10-05 16:36:08 +0900

    chapter1

---
+++ chapter1/main6.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARINGS
+#include <stdio.h>
+
+int main(void){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d + %d = %d\n", a, b, a + b);
+    printf("%d - %d = %d\n", a, b, a - b);
+    printf("%d * %d = %d\n", a, b, a * b);
+    printf("%d / %d = %d\n", a, b, a / b);
+    printf("%d %% %d = %d\n", a, b, a % b);
+    return 0;
+}
---
+++ chapter1/main7.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void){
+    printf("\"A B C D\"\n");  // "\"가 큰따옴표
+    printf("\"A\tB\tC\tD\"\n");
+}
