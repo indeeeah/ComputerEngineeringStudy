9a49f89256f5d71a75430f51a04b2adfb9352422
KIM-SOOMINI <soomin@e-flying.co>
2022-10-30 21:34:18 +0900

    c# interface

---
+++ part1_chapter4_7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2 {
+    public class Monster {
+        public string name;
+        public virtual void Attact() {
+            Console.WriteLine("공격 수행");
+        }
+    }
+
+    public class Ork : Monster {
+        public override void Attact() {
+            base.Attact();  // base는 부모클래스 , 공격수행 뒤 오크족장이 출력됨
+            name = "오크 족장";
+            Console.WriteLine(name + ": 오크 공격");
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Monster monster = new Monster();
+            monster.Attact();
+            Ork ork = new Ork();
+            ork.Attact();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ part1_chapter4_9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2 {
+    public interface Monster {
+        void Attack();
+        void Defence();
+    }
+    public class Ork : Monster {
+        public void Attack() {
+            Console.WriteLine("오크 공격");
+        }
+        public void Defence() {
+            Console.WriteLine("오크 방어");
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Ork ork = new Ork();
+            ork.Attack();
+            ork.Defence();
+            Console.ReadLine();
+        }
+    }
+}
